f716793fbc708fb16965bc6b3976dd1aa2ed05ec
SirPurpleness <sirpurpleness@gmail.com>
2013-12-28 23:51:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-
+	ini_set('display_errors', 1);
+	
 	//include the RainTPL class
 	include "inc/rain.tpl.class.php";
-    include "themes/theme-settings.php";
+    	include "themes/theme-settings.php";
 	include "inc/manage.tpl.class.php";
 
 	//initialize the template file
