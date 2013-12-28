2f598a8cf74e891fec7da2e24b95c17ad90453c1
SirPurpleness <sirpurpleness@gmail.com>
2013-12-28 23:48:06 +0100

    Security fix

--- admin/newpost.php
+++ admin/newpost.php
@@ -24,11 +24,34 @@ if ($rank_id == 7) {
     <div class="container">
         <div class="row-fluid">
             <div class="span12">
+				<?php
+					//Inserting Post to Database
+					if(isset($_POST['submit'])){
+					$dateposted = date("d/m/y");
+					$author_id = $user_id;
+					$post_title = $conn->real_escape_string($_POST['post_title']);
+					$post_content = $conn->real_escape_string($_POST['post_content']);
+
+					$addPost = $conn->query("INSERT INTO posts (dateposted, author_id, post_title, post_content) VALUES (
+					'" . $dateposted . "',
+					'" . $author_id . "',
+					'" . $post_title . "',
+					'" . $post_content . "')
+					");
+
+					if($addPost) {
+						echo "<div style='background: green; color:#FFF; border-radius:4px; margin-bottom: 15px; padding: 15px;'>Your post was successfully added.</div>";
+					} else {
+						echo "<div style='background: red; color:#FFF; border-radius:4px; margin-bottom: 15px; padding: 15px;'>" . printf($conn->error()) . "</div>";
+					}
+
+					}
+				?>
                 <div class="well well-small">
                     <strong>New Blog Post (<a href="blog.php">Go back</a>). </strong><br /><br />
                     <form class="form-horizontal" action="newpost.php" method="POST">
                     <input placeholder="Post Title" style="height: 40px;" type="text" class="input-large" name="post_title"><br /><br />
-                    <textarea name="post_content" style="width: 100%; height: 300px"></textarea><br />
+                    <textarea name="post_content" style="width: 100%; height: 300px; background: #FFF;"></textarea><br />
                     <button name="submit" value="submit" type="submit" class="btn btn-block btn-success">New Blog Post</button>
                     </form>
                 </div>
@@ -40,22 +63,6 @@ if ($rank_id == 7) {
 </html>
 <?php
 } else { header("Location: ../"); }
-
-//Inserting Post to Database
-if(isset($_POST['submit'])){
-$dateposted = date("d/m/y");
-$author_id = $user_id;
-$post_title = $_POST['post_title'];
-$post_content = $_POST['post_content'];
-
-$conn->query("INSERT INTO posts (dateposted, author_id, post_title, post_content) VALUES (
-'" . mysql_real_escape_string($dateposted) . "',
-'" . mysql_real_escape_string($author_id) . "',
-'" . mysql_real_escape_string($post_title) . "',
-'" . mysql_real_escape_string($post_content) . "')
-");
-header("Location: newpost.php");
-}
 
 $conn->close();
 ?>
