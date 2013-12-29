85bb8d161fb8d4d83fc16fb1301227c4622d74b1
SirPurpleness <sirpurpleness@gmail.com>
2013-12-29 21:32:44 +0100

    Huge update

--- inc/manage.tpl.class.php
+++ inc/manage.tpl.class.php
@@ -5,6 +5,7 @@
 
 //Get DB
 include 'admin/assets/database.php';
+include 'themes/theme-settings.php';
 raintpl::configure("base_url", null );
 raintpl::configure("tpl_dir", "themes/".$themeset."/" );
 raintpl::configure("cache_dir", "tmp/" );
@@ -23,10 +24,12 @@ while($row = $result->fetch_array())
 $tpl->assign( "sitename", $gsitename );
 //Getblog
 $blog_sql = $conn->query("SELECT DISTINCT * FROM posts ORDER BY post_id DESC LIMIT ".$gbloglimit."") or die(mysql_error()); 
-while ($db_field = $blog_sql->fetch_array()) {   
-    $test[] = $db_field;
+while($db_field = $blog_sql->fetch_array()) {
+	$test[] = $db_field;
 }
+
 $tpl->assign( "getblog", $test );
+
 // Get blog page
 $blog_page = $conn->query("SELECT * FROM posts ORDER BY post_id DESC LIMIT 10");
 $result = $blog_page->fetch_array();
@@ -43,6 +46,11 @@ $getBlogInfo = $conn->query("SELECT * FROM posts WHERE post_id = '".$blog_viewid
 $blogsview = $getBlogInfo->fetch_array();
 
 $tpl->assign("blogsview", $blogsview);
+
+if($getBlogInfo->num_rows <= 0) {
+	header("location: blog.php");
+}
+
 //Managing logged in users
 if (isset($_SESSION['logged'])) { 
     if ($_SESSION['logged'] == 1) {
@@ -57,6 +65,69 @@ if (isset($_SESSION['logged'])) {
         $tpl->assign( "rank_id", $rank_id );
         $tpl->assign( "username", $username );
     }
+}
+
+// Blog comments
+if(isset($_POST['submitComment'])) {
+	$blogComment = $conn->real_escape_string($_POST['blogComment']);
+	
+	if(!empty($blogComment)) {
+		$blogId = $conn->real_escape_string($_GET['id']);
+		$BlogUid = $conn->real_escape_string($username);
+		$blogDate = date("d/m/Y");
+		
+		$insertComment = $conn->query("INSERT INTO post_comments (post_id, user_name, comment, date) VALUES('".$blogId."', '".$BlogUid."', '".$blogComment."', '".$blogDate."')");
+		if($insertComment) {
+			header("location: blog.php?id=$blogId");
+		} else {
+			printf($conn->error());
+		}
+	} else {
+		$commentSign = "You can't leave the comment blank.";
+		$tpl->assign("commentSign", $commentSign);
+	}
+}
+
+// Show blog comments
+if(isset($_GET['id'])) { 
+	$cId = $conn->real_escape_string($_GET['id']);
+	$showBlogComments = $conn->query("SELECT * FROM post_comments WHERE post_id = '".$cId."' ORDER BY id");
+	while($fetchBlogComments = $showBlogComments->fetch_array()) {
+		$bComment[] = $fetchBlogComments;
+		$tpl->assign("getComments", $bComment);
+	}
+	
+	if(isset($_GET['reply'])) {
+		$rId = $conn->real_escape_string($_GET['reply']);
+		$tryRpl = $conn->query("SELECT * FROM post_comments WHERE id = '".$rId."'");
+		$fetchTryRpl = $tryRpl->fetch_array();
+		
+		$tpl->assign("fetchTryRpl", $fetchTryRpl);
+	}
+}
+
+//Blog Likes
+if(isset($_GET['id'])) {
+	if(isset($_SESSION['logged'])) {
+		if(isset($_POST['like'])) {
+			$addLike = $conn->query("UPDATE posts SET likes = likes + 1 WHERE post_id = '".$cId."'");
+			$addLock = $conn->query("INSERT INTO post_likes (user_id, post_id) VALUES('".$user_id."', '".$cId."')");
+		} elseif(isset($_POST['unlike'])) {
+			$removeLike = $conn->query("UPDATE posts SET likes = likes - 1 WHERE post_id = '".$cId."'");
+			$removeLock = $conn->query("DELETE FROM post_likes WHERE user_id = '".$user_id."' AND post_id = '".$cId."'");
+		}
+		
+		
+		$checkLock = $conn->query("SELECT * FROM post_likes WHERE user_id = '".$user_id."' AND post_id = '".$cId."'");
+		$controlLock = $checkLock->num_rows;
+		
+		
+		$tpl->assign("controlLock", $controlLock);
+	}
+	
+	$countLikes = $conn->query("SELECT * FROM post_likes WHERE post_id = '".$cId."'");
+	$getLikes = $countLikes->num_rows;
+	$tpl->assign("getLikes", $getLikes);
 }
 //Check logged
 
